855eff91c5138ee3bdc93cea6c21e12afcaf2e31
Michael Chalupiak <chalupmc@rose-hulman.edu>
2023-09-21 14:44:16 -0400

    follow along 4

--- 1.7/scripts/script.js
+++
@@ -1,7 +0,0 @@
-const h1 = document.querySelector('h1');
-const p = document.createElement('p');
-setTimeout(() => {
-    h1.parentElement.removeChild(h1);
-    p.textContent = 'Wildcard!';
-    document.body.appendChild(p);
-}, 5000);
