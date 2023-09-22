35b0335a5c06e32218b6bf7ba4f01b07954efc7f
Michael Chalupiak <chalupmc@rose-hulman.edu>
2023-09-22 10:21:34 -0400

    Homework 1

---
+++ public/homework1/scripts/main.js
@@ -0,0 +1,50 @@
+let counter = 0;
+let colorText = "Green"
+
+main = function() {
+     document.querySelector("#decrementButton").onclick = (event) => {
+        console.log("decrement button");
+        if (counter > 0) {
+            counter = counter - 1;
+        }
+         updateView();
+    };
+    document.querySelector("#resetButton").onclick = (event) => {
+        console.log("reset button");
+        counter = 0;
+        updateView();
+    };
+    document.querySelector("#incrementButton").onclick = (event) => {
+        console.log("increment button");
+        counter = counter + 1;
+        updateView();
+    };
+    document.querySelector("#blue").onclick = (event) => {
+        console.log("blue button");
+        colorText = "Blue";
+        updateView();
+    };
+    document.querySelector("#green").onclick = (event) => {
+        console.log("green button");
+        colorText = "Green";
+        updateView();
+    };
+    document.querySelector("#red").onclick = (event) => {
+        console.log("red button");
+        colorText = "Red";
+        updateView();
+    };
+    document.querySelector("#purple").onclick = (event) => {
+        console.log("purple button");
+        colorText = "Purple";
+        updateView();
+    };
+}
+
+updateView = function() {
+    document.querySelector("#counterText").innerHTML = `${counter}`;
+    document.querySelector("#colorBox").innerHTML = `${colorText}`;
+    document.getElementById("colorBox").style.backgroundColor = colorText.toLowerCase();
+}
+
+main();
